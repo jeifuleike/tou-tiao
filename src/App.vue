3f9d0aec014eed2e8903b26ff614ce812918eda7
jeifu <2572082102@qq.com>
2023-05-15 15:51:34 +0800

    初始化项目完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
+    <router-link></router-link>
   </div>
 </template>
+
+<script>
+export default {
+  name : 'App'
+}
+</script>
 
 <style>
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,7 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path:  '',
-    name:  '',
-    component: ''
-  },
-]
+const routes = []
 
 const router = new VueRouter({
   routes

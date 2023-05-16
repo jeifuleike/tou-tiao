28f468de8b70d100fa757d6b3875ab481cb7defb
jeifu <2572082102@qq.com>
2023-05-16 11:12:52 +0800

    基本工作准备完成

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <router-link></router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name : 'App'
+  name: 'App',
+  data() {
+    return {
+    };
+  }
 }
 </script>
 
-<style>
-</style>
+<style></style>

e0368300b93d5d362b2113705ba53069f8db80ae
jeifu <2572082102@qq.com>
2023-05-21 19:48:21 +0800

    登录模块完成

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { setItem,getItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
+const token_key = 'toutiao_user'
+
 export default new Vuex.Store({
   state: {
+    user: getItem(token_key)
   },
   getters: {
   },
   mutations: {
+    SetUser(state,data){
+      state.user = data
+      //为了防止刷新丢失，我们需要吧数据备份到本地存储
+     setItem(token_key,state.user)
+    }
   },
   actions: {
   },
